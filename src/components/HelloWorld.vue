e1b683d24225381b9e240b634a043494724a1adb
Umut ADALI <umutadali@teknasyon.com>
2019-10-04 02:49:24 +0300

    [Feat] Add navbar

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,23 @@
 <template>
-  <div class="bg-gray-800 hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+<body class="bg-gray-300">
+    <div class="mx-auto flex items-center py-3 justify-around bg-gray-200
+text-teal-800 shadow-md">
+      <ul class="list-none table-row">
+        <li class="table-cell border-solid border-r border-gray-400 "><a href="" class="text-sm pr-2 pl-2 shadow-sm"><i
+              class="fa fa-home fa-lg"></i></a></li>
+        <li class="table-cell pl-5 pr-5 border-solid border-r border-gray-400 "><a href=""
+            class="text-sm pr-2 pl-2 shadow-sm font-semibold">PORTALLAR</a></li>
+        <li class="table-cell pl-5 pr-5 border-solid border-r border-gray-400 "><a href=""
+            class="text-sm pr-2 pl-2 shadow-sm font-semibold">HIZLI MENU</a></li>
+        <li class="table-cell pl-5 pr-5 border-solid border-r border-gray-400 "><a href=""
+            class="text-sm pr-2 pl-2 shadow-sm font-semibold">HIZLI MENU</a></li>
+        <li class="table-cell pl-5 pr-5 border-solid border-r border-gray-400 "><a href=""
+            class="text-sm pr-2 pl-2 shadow-sm font-semibold">HIZLI MENU</a></li>
+        <li class="table-cell pl-5 pr-5 border-solid border-r border-gray-400 "><a href=""
+            class="text-sm pr-2 pl-2 shadow-sm font-semibold">HIZLI MENU</a></li>
+      </ul>
+    </div>
+</body>
 </template>
 
 <script>
@@ -37,22 +27,4 @@ export default {
     msg: String
   }
 }
-</script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
